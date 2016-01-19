150009def913bb1161c18106f4ad17840df7b5dd
Oskar Radon and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-19 10:36:46 -0800

    made leap year detector work

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
+// function to calculate leap year
+var leapYear = function(year) {
+  return ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0));
+}
+
+
 $ (document).ready(function () {
   $ ("#yearForm").submit(function(event) {
-    var year = $("input#inputLeapYear").val();
+    var input = parseInt($("input#inputLeapYear").val());
 
-    // function to calculate leap year
-    var leapYear = function(year) {
-      return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
-    };
-    //
-    // console.log (leapYear(year));
-    $("#output").append(leapYear(year));
+    console.log(leapYear(input));
+    $("#output").text(leapYear(input));
 
     event.preventDefault();
   });
